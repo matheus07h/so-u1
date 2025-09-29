10157b9412d0a100e10d85d30e709b5d3bc884a9
RafaelFernandes12 <rfsilva786@gmail.com>
2025-09-29 16:14:25 -0300

    feat(data): add partial matrix multiplication results
    Add the file `resultado_processos_parte_0.txt` containing partial results
    of matrix multiplication processed in parallel. This file stores the
    output of a batch of calculations, likely as part of a larger
    parallelized computation using processes. The addition supports
    incremental result storage and further aggregation or analysis.

--- processos.cpp
+++ processos.cpp
@@ -1,56 +1,85 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+#include <algorithm>
 #include <chrono>
-
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
 using namespace std;
 
+// Supondo que as matrizes e variáveis já foram lidas antes deste trecho
+// Exemplo de declaração (ajuste conforme necessário):
+// int n1, m1, n2, m2, p;
+// vector<vector<int>> matriz1, matriz2;
+
 int main(int argc, char *argv[]) {
-    ifstream arq1(argv[1]);
-    ifstream arq2(argv[2]);
-    int p = atoi(argv[3]);
 
-    int n1, m1, n2, m2;
-    arq1 >> n1 >> m1;
-    arq2 >> n2 >> m2;
-   
-    vector<vector<int>> matriz1(n1, vector<int>(m1));
-    vector<vector<int>> matriz2(n2, vector<int>(m2));
-    vector<vector<int>> resultado(n1, vector<int>(m2, 0));
+  if (argc != 4) {
+    cerr << "Uso: " << argv[0] << " <matriz1.txt> <matriz2.txt> <P>\n";
+    return 1;
+  }
 
-    if (m1 != n2) {
-        cerr << "Multiplicação impossível: m1 != n2\n";
-        return 1;
-    }
+  ifstream fin1(argv[1]);
+  int n1, m1;
+  fin1 >> n1 >> m1;
+  vector<vector<int>> matriz1(n1, vector<int>(m1));
+  for (int i = 0; i < n1; ++i)
+    for (int j = 0; j < m1; ++j)
+      fin1 >> matriz1[i][j];
+  fin1.close();
 
-    // Lendo a matriz1
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < m1; j++) {
-            arq1 >> matriz1[i][j];
+  // Leitura das dimensões e valores da matriz 2
+  ifstream fin2(argv[2]);
+  int n2, m2;
+  fin2 >> n2 >> m2;
+  vector<vector<int>> matriz2(n2, vector<int>(m2));
+  for (int i = 0; i < n2; ++i)
+    for (int j = 0; j < m2; ++j)
+      fin2 >> matriz2[i][j];
+  fin2.close();
+
+  int p = stoi(argv[3]);
+
+  auto begin = chrono::steady_clock::now();
+
+  int total_elements = n1 * m2;
+  int num_parts = (total_elements + p - 1) / p;
+
+  for (int part = 0; part < num_parts; ++part) {
+    int start = part * p;
+    int end = min(start + p, total_elements);
+    pid_t pid = fork();
+    if (pid == 0) { // child
+      auto t0 = chrono::steady_clock::now();
+      ofstream fout("data/processos/resultado_parte_" + to_string(part) +
+                    ".txt");
+      for (int idx = start; idx < end; ++idx) {
+        int i = idx / m2;
+        int j = idx % m2;
+        int val = 0;
+        for (int k = 0; k < m1; ++k) {
+          val += matriz1[i][k] * matriz2[k][j];
         }
+        fout << "c" << setfill('0') << setw(8) << i << setw(2) << j << " "
+             << val << "\n";
+      }
+      auto t1 = chrono::steady_clock::now();
+      long tempo = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+      fout << tempo << "\n";
+      fout.close();
+      exit(0);
     }
-
-    arq1.close();
-
-    // Lendo a matrix2
-    for (int i = 0; i < n2; i++) {
-        for (int j = 0; j < m2; j++) {
-            arq2 >> matriz2[i][j];
-        }
-    }
-
-    arq2.close();
-
-    // Multiplicação
-    auto begin = chrono::steady_clock::now();
-
-    // Implementação com processos (p processos)
-
-    auto end = chrono::steady_clock::now();
-
-    long tempoExecucao = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-
-    // Salvar os resultados
-
-    return 0;
+  }
+  // Espera todos os filhos terminarem
+  for (int part = 0; part < num_parts; ++part) {
+    wait(nullptr);
+  }
+  auto end = chrono::steady_clock::now();
+  long tempoExecucao =
+      chrono::duration_cast<chrono::milliseconds>(end - begin).count();
+  cout << "Tempo total(ms): " << tempoExecucao << endl;
+  return 0;
 }
--- threads.cpp
+++ threads.cpp
@@ -1,56 +1,100 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
+// Implementação com threads (p threads)
+#include <algorithm>
 #include <chrono>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    ifstream arq1(argv[1]);
-    ifstream arq2(argv[2]);
-    int p = atoi(argv[3]);
+  if (argc != 4) {
+    cerr << "Uso: " << argv[0] << " <matriz1.txt> <matriz2.txt> <P>\n";
+    return 1;
+  }
 
-    int n1, m1, n2, m2;
-    arq1 >> n1 >> m1;
-    arq2 >> n2 >> m2;
-   
-    vector<vector<int>> matriz1(n1, vector<int>(m1));
-    vector<vector<int>> matriz2(n2, vector<int>(m2));
-    vector<vector<int>> resultado(n1, vector<int>(m2, 0));
+  // Leitura dos argumentos
+  string file1 = argv[1];
+  string file2 = argv[2];
+  int p = stoi(argv[3]);
 
-    if (m1 != n2) {
-        cerr << "Multiplicação impossível: m1 != n2\n";
-        return 1;
+  // Leitura da matriz 1
+  ifstream fin1(file1);
+  int n1, m1;
+  fin1 >> n1 >> m1;
+  vector<vector<int>> matriz1(n1, vector<int>(m1));
+  for (int i = 0; i < n1; ++i)
+    for (int j = 0; j < m1; ++j)
+      fin1 >> matriz1[i][j];
+  fin1.close();
+
+  // Leitura da matriz 2
+  ifstream fin2(file2);
+  int n2, m2;
+  fin2 >> n2 >> m2;
+  vector<vector<int>> matriz2(n2, vector<int>(m2));
+  for (int i = 0; i < n2; ++i)
+    for (int j = 0; j < m2; ++j)
+      fin2 >> matriz2[i][j];
+  fin2.close();
+
+  if (m1 != n2) {
+    cerr << "Erro: Matrizes incompatíveis para multiplicação.\n";
+    return 1;
+  }
+
+  vector<vector<int>> resultado(n1, vector<int>(m2, 0));
+  mutex file_mutex;
+  int total_elements = n1 * m2;
+  int num_parts = (total_elements + p - 1) / p;
+
+  auto compute_and_save = [&](int part_idx, int start_idx, int end_idx) {
+    auto t0 = chrono::steady_clock::now();
+    vector<pair<int, int>> positions;
+    for (int idx = start_idx; idx < end_idx; ++idx) {
+      int i = idx / m2;
+      int j = idx % m2;
+      resultado[i][j] = 0;
+      for (int k = 0; k < m1; ++k) {
+        resultado[i][j] += matriz1[i][k] * matriz2[k][j];
+      }
+      positions.emplace_back(i, j);
     }
+    auto t1 = chrono::steady_clock::now();
+    long tempo = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
 
-    // Lendo a matriz1
-    for (int i = 0; i < n1; i++) {
-        for (int j = 0; j < m1; j++) {
-            arq1 >> matriz1[i][j];
-        }
+    string fname =
+        "data/threads/resultado_parte_" + to_string(part_idx) + ".txt";
+    lock_guard<mutex> lock(file_mutex);
+    ofstream fout(fname);
+    for (auto &pos : positions) {
+      int i = pos.first;
+      int j = pos.second;
+      fout << "c" << setfill('0') << setw(8) << i << setw(2) << j << " "
+           << resultado[i][j] << "\n";
     }
+    fout << "Tempo(ms): " << tempo << "\n";
+    fout.close();
+  };
 
-    arq1.close();
+  auto total_t0 = chrono::steady_clock::now();
+  vector<thread> threads;
+  for (int part = 0; part < num_parts; ++part) {
+    int start = part * p;
+    int end = min(start + p, total_elements);
+    threads.emplace_back(compute_and_save, part, start, end);
+  }
+  for (auto &t : threads)
+    t.join();
 
-    // Lendo a matrix2
-    for (int i = 0; i < n2; i++) {
-        for (int j = 0; j < m2; j++) {
-            arq2 >> matriz2[i][j];
-        }
-    }
-
-    arq2.close();
-
-    // Multiplicação
-    auto begin = chrono::steady_clock::now();
-
-    // Implementação com threads (p threads)
-
-    auto end = chrono::steady_clock::now();
-
-    long tempoExecucao = chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-
-    // Salvar os resultados
-
-    return 0;
+  auto total_t1 = chrono::steady_clock::now();
+  long total_ms =
+      chrono::duration_cast<chrono::milliseconds>(total_t1 - total_t0).count();
+  cout << "Tempo total(ms): " << total_ms << endl;
+  return 0;
 }
