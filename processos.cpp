254c5943f7388b49dcae545fc75917dd50f65cce
RafaelFernandes12 <rfsilva786@gmail.com>
2025-10-01 01:08:36 -0300

    feat(data): add initial processos result dataset
    Added the initial version of the `resultado_parte_0.txt` dataset under
    `data/processos/` containing process results. Also updated `.gitignore`
    to exclude `.exe` and `.txt` files from version control. This dataset
    will be used for further analysis and processing in the project.

--- processos.cpp
+++ processos.cpp
@@ -8,79 +8,76 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <vector>
-using namespace std;
 
-// Supondo que as matrizes e variáveis já foram lidas antes deste trecho
-// Exemplo de declaração (ajuste conforme necessário):
-// int n1, m1, n2, m2, p;
-// vector<vector<int>> matriz1, matriz2;
+using namespace std;
+void read_matrix(const string &filename, vector<vector<int>> &matrix, int &rows,
+                 int &cols) {
+  ifstream fin(filename);
+  fin >> rows >> cols;
+  matrix.resize(rows, vector<int>(cols));
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      fin >> matrix[i][j];
+  fin.close();
+}
+
+void compute(const vector<vector<int>> &matriz1,
+             const vector<vector<int>> &matriz2, int m1, int m2, int start,
+             int end, int part) {
+  auto t0 = chrono::steady_clock::now();
+  ofstream fout("data/processos/resultado" + to_string(part) + ".txt");
+
+  for (int index = start; index < end; ++index) {
+    int i = index / m2;
+    int j = index % m2;
+    int val = 0;
+    for (int k = 0; k < m1; ++k) {
+      val += matriz1[i][k] * matriz2[k][j];
+    }
+    fout << "c" << setfill('0') << setw(8) << i << setw(2) << j << " " << val
+         << "\n";
+  }
+  auto t1 = chrono::steady_clock::now();
+  long tempo = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+  fout << tempo << "\n";
+  fout.close();
+}
 
 int main(int argc, char *argv[]) {
 
-  if (argc != 4) {
-    cerr << "Uso: " << argv[0] << " <matriz1.txt> <matriz2.txt> <P>\n";
+  if (argc != 2) {
+    cerr << "Uso: " << argv[0] << " <P>\n";
     return 1;
   }
 
-  ifstream fin1(argv[1]);
-  int n1, m1;
-  fin1 >> n1 >> m1;
-  vector<vector<int>> matriz1(n1, vector<int>(m1));
-  for (int i = 0; i < n1; ++i)
-    for (int j = 0; j < m1; ++j)
-      fin1 >> matriz1[i][j];
-  fin1.close();
+  vector<vector<int>> matriz1, matriz2;
+  int n1, m1, n2, m2;
+  read_matrix("matriz1.txt", matriz1, n1, m1);
+  read_matrix("matriz2.txt", matriz2, n2, m2);
 
-  // Leitura das dimensões e valores da matriz 2
-  ifstream fin2(argv[2]);
-  int n2, m2;
-  fin2 >> n2 >> m2;
-  vector<vector<int>> matriz2(n2, vector<int>(m2));
-  for (int i = 0; i < n2; ++i)
-    for (int j = 0; j < m2; ++j)
-      fin2 >> matriz2[i][j];
-  fin2.close();
+  int p = stoi(argv[1]);
 
-  int p = stoi(argv[3]);
-
-  auto begin = chrono::steady_clock::now();
-
+  system("rm -rf data/processos/*");
   int total_elements = n1 * m2;
   int num_parts = (total_elements + p - 1) / p;
-  cout << num_parts << " partes\n";
 
+  auto t0 = chrono::steady_clock::now();
   for (int part = 0; part < num_parts; ++part) {
     int start = part * p;
     int end = min(start + p, total_elements);
     pid_t pid = fork();
-    if (pid == 0) { // child
-      auto t0 = chrono::steady_clock::now();
-      ofstream fout("data/processos/resultado_parte_" + to_string(part) +
-                    ".txt");
-      for (int idx = start; idx < end; ++idx) {
-        int i = idx / m2;
-        int j = idx % m2;
-        int val = 0;
-        for (int k = 0; k < m1; ++k) {
-          val += matriz1[i][k] * matriz2[k][j];
-        }
-        fout << "c" << setfill('0') << setw(8) << i << setw(2) << j << " "
-             << val << "\n";
-      }
-      auto t1 = chrono::steady_clock::now();
-      long tempo = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
-      fout << tempo << "\n";
-      fout.close();
+    if (pid == 0) {
+      compute(matriz1, matriz2, m1, m2, start, end, part);
       exit(0);
     }
   }
-  // Espera todos os filhos terminarem
+
   for (int part = 0; part < num_parts; ++part) {
     wait(nullptr);
   }
-  auto end = chrono::steady_clock::now();
-  long tempoExecucao =
-      chrono::duration_cast<chrono::milliseconds>(end - begin).count();
-  cout << "Tempo total(ms): " << tempoExecucao << endl;
+
+  auto t1 = chrono::steady_clock::now();
+  long ms = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+  cout << "Execution: " << ms << " ms" << endl;
   return 0;
 }
--- threads.cpp
+++ threads.cpp
@@ -1,101 +1,74 @@
-// Implementação com threads (p threads)
 #include <algorithm>
 #include <chrono>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
-// #include <mutex>
-// #include <utility>
 #include <string>
 #include <thread>
 #include <vector>
 
 using namespace std;
 
+void read_matrix(const string &filename, vector<vector<int>> &matrix, int &rows,
+                 int &cols) {
+  ifstream fin(filename);
+  fin >> rows >> cols;
+  matrix.resize(rows, vector<int>(cols));
+  for (int i = 0; i < rows; ++i)
+    for (int j = 0; j < cols; ++j)
+      fin >> matrix[i][j];
+  fin.close();
+}
+
+void compute(const vector<vector<int>> &matrix1,
+             const vector<vector<int>> &matrix2, int cols1, int cols2,
+             int start_idx, int end_idx, int thread_id) {
+  auto t0 = chrono::steady_clock::now();
+  ofstream fout("data/threads/result" + to_string(thread_id) + ".txt");
+
+  for (int index = start_idx; index < end_idx; ++index) {
+    int i = index / cols2;
+    int j = index % cols2;
+    int val = 0;
+    for (int k = 0; k < cols1; ++k) {
+      val += matrix1[i][k] * matrix2[k][j];
+    }
+    fout << "c" << setfill('0') << setw(8) << i << setw(4) << j << " " << val
+         << "\n";
+  }
+  auto t1 = chrono::steady_clock::now();
+  long ms = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+  fout << ms << "\n";
+}
+
 int main(int argc, char *argv[]) {
-  if (argc != 4) {
-    cerr << "Uso: " << argv[0] << " <matriz1.txt> <matriz2.txt> <P>\n";
-    return 1;
-  }
-
-  string file1 = argv[1];
-  string file2 = argv[2];
-  int p = stoi(argv[3]);
-
-  ifstream fin1(file1);
-  int n1, m1;
-  fin1 >> n1 >> m1;
-  vector<vector<int>> matriz1(n1, vector<int>(m1));
-  for (int i = 0; i < n1; ++i)
-    for (int j = 0; j < m1; ++j)
-      fin1 >> matriz1[i][j];
-  fin1.close();
-
-  ifstream fin2(file2);
-  int n2, m2;
-  fin2 >> n2 >> m2;
-  vector<vector<int>> matriz2(n2, vector<int>(m2));
-  for (int i = 0; i < n2; ++i)
-    for (int j = 0; j < m2; ++j)
-      fin2 >> matriz2[i][j];
-  fin2.close();
-
-  if (m1 != n2) {
-    cerr << "Erro: Matrizes incompatíveis para multiplicação.\n";
-    return 1;
-  }
+  string matrix_file1 = "matriz1.txt";
+  string matrix_file2 = "matriz2.txt";
+  int p = stoi(argv[1]);
+  system("rm -rf data/threads/*");
+  vector<vector<int>> matrix1, matrix2;
+  int n1, m1, n2, m2;
+  read_matrix(matrix_file1, matrix1, n1, m1);
+  read_matrix(matrix_file2, matrix2, n2, m2);
 
   int total_elements = n1 * m2;
-  cout << total_elements << " elementos na matriz resultante.\n";
-  if (p <= 0) {
-    cerr << "Erro: P deve ser maior que zero.\n";
-    return 1;
-  }
-  if (p > total_elements) {
-    p = total_elements;
-  }
 
   int num_threads = (total_elements + p - 1) / p;
-  cout << num_threads << " threads disponíveis.\n";
+  auto t0 = chrono::steady_clock::now();
 
-  // Vetor para armazenar os resultados (opcional, pode ser removido)
-  // vector<vector<int>> resultado(n1, vector<int>(m2, 0));
-
-  auto compute = [&](int start_idx, int end_idx, int thread_id) {
-    auto t0 = chrono::steady_clock::now();
-    ofstream fout("data/threads/resultado_partes_" + to_string(thread_id) +
-                  ".txt");
-    for (int idx = start_idx; idx < end_idx; ++idx) {
-      int i = idx / m2;
-      int j = idx % m2;
-      int val = 0;
-      for (int k = 0; k < m1; ++k) {
-        val += matriz1[i][k] * matriz2[k][j];
-      }
-      // resultado[i][j] = val;
-      fout << "c" << setfill('0') << setw(8) << i << setw(4) << j << " " << val
-           << "\n";
-    }
-    auto t1 = chrono::steady_clock::now();
-    long ms = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
-    fout << ms << "\n";
-  };
-
-  auto total_t0 = chrono::steady_clock::now();
   vector<thread> threads;
   for (int t = 0; t < num_threads; ++t) {
-    int start = t * (total_elements / num_threads) +
-                min(t, total_elements % num_threads);
-    int end = start + (total_elements / num_threads) +
-              (t < (total_elements % num_threads) ? 1 : 0);
-    threads.emplace_back(compute, start, end, t);
+    int start = t * p;
+    int end = min(start + p, total_elements);
+    threads.emplace_back(compute, cref(matrix1), cref(matrix2), m1, m2, start,
+                         end, t);
   }
+
   for (auto &th : threads)
     th.join();
 
-  auto total_t1 = chrono::steady_clock::now();
-  long total_ms =
-      chrono::duration_cast<chrono::milliseconds>(total_t1 - total_t0).count();
-  cout << "Tempo total(ms): " << total_ms << endl;
+  auto t1 = chrono::steady_clock::now();
+  long ms = chrono::duration_cast<chrono::milliseconds>(t1 - t0).count();
+  cout << "Execution: " << ms << " ms" << endl;
   return 0;
 }
